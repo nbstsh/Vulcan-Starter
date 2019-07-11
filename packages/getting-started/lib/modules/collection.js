c596f65d2ea3336f04298e0b3302fb98bafcbbc4
nbstsh <nbstsh@gmail.com>
2019-07-12 03:58:37 +0900

    [starter] step6

--- packages/getting-started/lib/modules/collection.js
+++ packages/getting-started/lib/modules/collection.js
@@ -4,7 +4,11 @@ The main Movies collection definition file.
 
 */
 
-import { createCollection, getDefaultResolvers, getDefaultMutations } from 'meteor/vulcan:core';
+import {
+  createCollection,
+  getDefaultResolvers,
+  getDefaultMutations
+} from 'meteor/vulcan:core';
 import schema from './schema.js';
 
 let Movies;
@@ -17,18 +21,16 @@ Uncomment on #Step6:
 
 */
 
-// Movies = createCollection({
+Movies = createCollection({
+  collectionName: 'Movies',
 
-//   collectionName: 'Movies',
+  typeName: 'Movie',
 
-//   typeName: 'Movie',
+  schema
 
-//   schema,
-  
-//   // resolvers: getDefaultResolvers('Movies'), // Uncomment on #Step9
+  // resolvers: getDefaultResolvers('Movies'), // Uncomment on #Step9
 
-//   // mutations: getDefaultMutations('Movies'), // Uncomment on #Step14
-
-// });
+  // mutations: getDefaultMutations('Movies'), // Uncomment on #Step14
+});
 
 export default Movies;
