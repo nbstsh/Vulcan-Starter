fe90a44a1561753ee3630f83246a048ad200f1ee
nbstsh <nbstsh@gmail.com>
2019-07-12 03:50:06 +0900

    [starter] step4

--- packages/getting-started/lib/components/steps/Step4.jsx
+++ packages/getting-started/lib/components/steps/Step4.jsx
@@ -20,13 +20,11 @@ As we said previously, in Vulcan any registered component can be accessed as \`<
 // uncomment the component's children on #Step4:
 
 const Step4 = () => (
-  <Components.Step step={4} text={text} after={after}>
-    {/* 
-    <Components.ModalTrigger component={<a href="#">Click Me!</a>}>
-      <div>Hello World!</div>
-    </Components.ModalTrigger> 
-    */}
-  </Components.Step>
+	<Components.Step step={4} text={text} after={after}>
+		<Components.ModalTrigger component={<a href='#'>Click Me!</a>}>
+			<div>Hello World!</div>
+		</Components.ModalTrigger>
+	</Components.Step>
 );
 
 registerComponent({ name: 'Step4', component: Step4 });
